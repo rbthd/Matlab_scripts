225d8ed0fdc82461f7a9f52cbf37d3fd9a82d602
rbthd <robin.therond@wanadoo.fr>
2018-02-14 12:48:30 +0100

    Improving scripts

--- integration.m
+++ integration.m
@@ -3,37 +3,51 @@
 clc; %reset command window
 clearvars; %reset variables
 results=importdata('/Users/robintherond/Documents/Polytech/PIFE/instantWave/Tests/test_barometer_slow_rob.csv', ' ');
+results2=importdata('/Users/robintherond/Documents/Polytech/PIFE/instantWave/Tests/test_goUp.csv', ' ');
+
 scrsz = get(groot,'ScreenSize');
+live = false; %Flag to enable animation
 
 samplePer = 0.02; %s
 
 orderButter = 2; %order of the HP filter
-orderPolyBaro = 10;
-orderButterBaro = 2;
+
+orderPolyBaro1 = 10;
+orderButterBaro1 = 2;
+
+orderPolyBaro2 = 10;
+orderButterBaro2 = 2;
 
 filtCutOff = 0.3; %cut off frequency of HP filter
-filtCutOffLP = 1;
+filtCutOffLP1 = 1;
+filtCutOffLP2 = 1;
+
 SamplePlotFreq = 2; %For the Animation
 
 length_data = length(results);
 
-acc=[results(:,26),results(:,27),results(:,28)];
-gyr=[results(:,29),results(:,30),results(:,31)];
-baro=results(:,36);
+%Data from first file
+acc1=[results(:,26),results(:,27),results(:,28)];
+gyr1=[results(:,29),results(:,30),results(:,31)];
+baro1=results(:,36);
 
-R = zeros(3,3,length(gyr));     % rotation matrix describing sensor relative to Earth
+%Data from second file
+baro2=results2(:,36);
+
+R = zeros(3,3,length(gyr1));     % rotation matrix describing sensor relative to Earth
 
 interv=0.02:0.02:0.02*length(results(:,36));
+interv2=0.02:0.02:0.02*length(results2(:,36));
 
 %reference function of motion
-h0=baro(1); %First barometer output -> altitude reference
+h0=baro1(1); %First barometer output -> altitude reference
 height_ref=h0+0.08*sin(2*pi*(1/7.75)*interv);
 
 
 ahrs = MahonyAHRS('SamplePeriod', samplePer, 'Kp', 1);
 
-for i = 1:length(gyr)
-    ahrs.UpdateIMU(gyr(i,:) * (pi/180), acc(i,:));	% gyroscope units must be radians
+for i = 1:length(gyr1)
+    ahrs.UpdateIMU(gyr1(i,:) * (pi/180), acc1(i,:));	% gyroscope units must be radians
     R(:,:,i) = quatern2rotMat(ahrs.Quaternion)';    % transpose because ahrs provides Earth relative to sensor
 end
 
@@ -46,22 +60,27 @@ end
 % end
 
 
-linAcc = ComputelinAcc(acc);
+linAcc = ComputelinAcc(acc1);
 linVel = BasicIntAcc(linAcc, samplePer);
 linVelHP = HPFilter(linVel,orderButter,filtCutOff,samplePer);
 linPos = BasicIntVel(linVel, samplePer);
 linPosHP = HPFilter(linPos,orderButter,filtCutOff,samplePer);
 
 %plotAcc(interv,acc);
-plotLinAcc(interv,linAcc);
+%plotLinAcc(interv,linAcc);
 %plotLinVel(interv,linVel);
 %plotLinVelHP(interv,linVelHP);
 %plotLinPos(interv,linPos);
 plotLinPosHP(interv,linPosHP,height_ref);
 
-%plotBarometer(baro,interv, orderPolyBaro, orderButterBaro, filtCutOffLP, samplePer)
 
+live=false;
+ref=false;
+plotBarometer(baro2,interv2, orderPolyBaro2, orderButterBaro2, filtCutOffLP2, samplePer, live, ref)
 
+live=true;
+ref=true;
+plotBarometer(baro1,interv, orderPolyBaro1, orderButterBaro1, filtCutOffLP1, samplePer, live, ref)
 
 
 % SixDOFanimation(linPosHP, R, ...
@@ -174,7 +193,8 @@ end
 %% Plot High-pass filtered Linear Position
 function plotLinPosHP(interv, linPosHP, f)
 % Plot
-figure('NumberTitle', 'off', 'Name', 'High-pass filtered Linear Position');
+scrsz = get(groot,'ScreenSize');
+figure('NumberTitle', 'off', 'Name', 'High-pass filtered Linear Position','Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)]);
 hold on;
 plot(interv,linPosHP(:,1), 'r', 'LineWidth',2);
 plot(interv,linPosHP(:,2), 'g', 'LineWidth',2);
@@ -190,7 +210,7 @@ legend('X', 'Y', 'Z','Reference function','Offsted Reference function');
 axis([0 23 -0.8 0.4])
 hold off;
 
-figure('Name','Resampling');
+figure('Name','Resampling','Position',[scrsz(3)/2 scrsz(4)/2 scrsz(3)/2 scrsz(4)]);
 plot(interv,f,'c','LineWidth',3); %Ref func
 x=0.05:0.05:0.05*length(linPosHP);
 hold on;
@@ -208,39 +228,84 @@ end
 
 %% Plot barometer outputs
 
-function plotBarometer(baro,interv,orderPolyBaro, orderButterBaro, filtCutOffLP, samplePer)
-scrsz = get(groot,'ScreenSize');
-figure('Name','Altitude from barometer','Position',[1 scrsz(4)/2 scrsz(4) scrsz(4)]);
-hold on;
+function plotBarometer(baro,interv,orderPolyBaro, orderButterBaro, filtCutOffLP, samplePer, live, ref)
 
 %REFERENCE Function 
 h0=baro(1); %First barometer output -> altitude reference
 height_ref=h0+0.1*sin(2*pi*(1/7.75)*interv);
-plot(interv,height_ref,'m*'); %Reference function
-
-%Unfiltered data
-plot(interv,baro,'Color','k'); %Unfiltered output
 
 %LOW PASS Filter
 altitudeLP = LPFilter(baro, orderButterBaro, filtCutOffLP, samplePer);
-plot(interv,altitudeLP,'Color','b','LineWidth',3); %Low pass filtered output
 
 %Sliding average
 for i = 1:length(altitudeLP)
 baro_avg(i) = mean(altitudeLP(max([1 i-50]):i));
 end
-plot(interv,baro_avg,'Color','y','LineWidth',4); %Averaged output
 
 
 %Polynomial extrapolation
 [p,~,mu] = polyfit(interv,baro_avg,orderPolyBaro); %scaling and centering
 f=polyval(p,interv,[],mu);
+
+scrsz = get(groot,'ScreenSize');
+figure('Name','Altitude from barometer','Position',[1 scrsz(4)/2 scrsz(3) scrsz(4)]);
+hold on;
+
+if(ref)
+    %Plot Reference function
+    plot(interv,height_ref,'m*'); %Reference function
+end
+%Plot RAW data
+plot(interv,baro,'Color','k'); %Unfiltered output
+%Plot Low-pass filtered
+plot(interv,altitudeLP,'Color','b','LineWidth',3); %Low pass filtered output
+%Plot sliding average
+plot(interv,baro_avg,'Color','y','LineWidth',4); %Averaged output
+%Plot polynomial
 plot(interv,f,'LineWidth',4,'Color','r');
+
 xlabel('Time (s)');
 ylabel('Height (m)');
-
-
 legend({'Unfiltered','Low pass filtered','Sliding average','Polynomial extrapolation'},'FontSize',15);
 hold off;
 
+if (live)
+%Plot Animation
+scrsz = get(groot,'ScreenSize');
+figure('Name','Live evolution','Position',[1 scrsz(4)/2 scrsz(3) scrsz(4)]);
+
+t = 1 ;
+x1 = 1 ;
+x2 = 1;
+x3 = 3;
+x4 = 1;
+startSpot = 1;
+interv = 1060 ; % considering 1000 samples
+step = 5 ; % lowering step has a number of cycles and then acquire more data
+hold on;
+while ( t <interv )
+    b = baro(t);
+    c = altitudeLP(t);
+    d = f(t);
+    e = height_ref(t)
+    x1 = [ x1, b ];
+    x2 = [ x2, c ];
+    x3 = [ x3, d ];
+    x4 = [ x4, e ];
+    plot(x1,'k') ;
+    plot(x2,'g');
+    plot(x3,'r');
+    plot(x4,'m');
+      if ((t/step)-50 < 0)
+          startSpot = 0;
+      else
+          startSpot = (t/step)-50;
+      end
+      axis([ startSpot, (t/step+30), -1 , 0.7 ]);
+      grid on
+      t = t + step;
+      drawnow;
+      pause(0.01);
+  end
+end
 end
